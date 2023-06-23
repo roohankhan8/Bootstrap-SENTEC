4fd4151906c8988d1a645dd9721e632d17886886
Roohan Khan <roohank8@gmail.com>
2023-06-23 18:02:34 +0500

    Third

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 $(function () {
   $("#nav").load("navigation.html");
   $("#footer").load("footer.html");
+  $("#bootstrap").load("bootstrap.html");
 });
+
+// let services = $('#services')
+// let contact = $('#contact')
+// let pricing = $('#pricing')
 
 function removeClass() {
   document.getElementById('services').classList.remove("active");
   document.getElementById('pricing').classList.remove("active");
   document.getElementById('contact').classList.remove("active");
 }
+
 function active(ele) {
   removeClass()
   document.getElementById(ele).classList.add("active");
 }
 
-// $(document).ready(function() {
-
-//   $("#myElement").click(function() {
-//     $(this).toggleClass("myClass");
-//   });
-// });
+$(document).ready(function() {
+  $("#services").click(function() {
+    $(this).addClass("active");
+  });
+});
